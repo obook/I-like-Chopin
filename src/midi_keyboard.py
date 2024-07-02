c2abf04d025ef7aeb1e409847b1d7fdfac0f0dd1
obook <obooklage@free.fr>
2024-07-02 12:08:17 +0200

    More midifiles and licence

--- src/midi_keyboard.py
+++ src/midi_keyboard.py
@@ -3,7 +3,7 @@
 # if __name__ == "__main__":
 #     pass
 
-import mido
+from mido import open_input
 
 # Wait keys from keyboard
 def ThreadKeyBoard(in_device, keys, pParent):
@@ -12,7 +12,7 @@ def ThreadKeyBoard(in_device, keys, pParent):
     keys['ThreadKeyBoard'] = True
 
     # NON-BLOCKING
-    inport = mido.open_input(in_device)
+    inport = open_input(in_device)
     print(f'Wait keys from "{in_device}...')
     while True: # non-blocking
         if not keys['run']:
@@ -24,24 +24,24 @@ def ThreadKeyBoard(in_device, keys, pParent):
         for key in inport.iter_pending():
 
             if key.type == 'note_on':
-                keys['note_on'] +=1
+                keys['key_on'] +=1
                 # Security ; press key C#4 (49) for pause
                 if key.note == 49 :
-                    keys['note_on'] = 0
+                    keys['key_on'] = 0
 
             elif key.type == 'note_off':
-                keys['note_on'] -=1
+                keys['key_on'] -=1
 
-            if keys['note_on'] <0 : # rare, in case of missing key on
-                keys['note_on'] = 0
+            if keys['key_on'] <0 : # rare, in case of missing key on
+                keys['key_on'] = 0
 
 
-            #print(f"keys on:{keys['note_on']}\r", end="")
-            pParent.PrintKeys(str(keys['note_on']))
+            #print(f"keys on:{keys['key_on']}\r", end="")
+            pParent.PrintKeys(str(keys['key_on']))
 
-            #if key.type == 'note_on' or key.type == 'note_off':
+            #if key.type == 'key_on' or key.type == 'note_off':
             #    note, octave = number_to_note(key.note)
-            #    print(f"{key.type} {note}{octave} ({key.note}) [{keys['note_on']} keys on]")
+            #    print(f"{key.type} {note}{octave} ({key.note}) [{keys['key_on']} keys on]")
 
     '''
     # BLOCKING
@@ -57,20 +57,20 @@ def ThreadKeyBoard(in_device, keys, pParent):
                     return
 
                 elif key.type == 'note_on':
-                    keys['note_on'] +=1
+                    keys['key_on'] +=1
                     # note, octave = number_to_note(key.note)
                     # print(f"{key.note}={note}{octave}")
                     # Security ; press key C#4 (49) for pause
                     if key.note == 49 :
-                        keys['note_on'] = 0
+                        keys['key_on'] = 0
 
                 elif key.type == 'note_off':
-                    keys['note_on'] -=1
+                    keys['key_on'] -=1
 
-                if keys['note_on'] <0 : # rare, in case of missing key on
-                    keys['note_on'] = 0
+                if keys['key_on'] <0 : # rare, in case of missing key on
+                    keys['key_on'] = 0
 
-                print(f"keys on:{keys['note_on']}\r", end="")
+                print(f"keys on:{keys['key_on']}\r", end="")
     except:
         print(f'Error connect to input "{in_device}"')
   '''
