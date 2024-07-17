03e29b61018ab78d47ca3614440a1ccaea1bb0a2
obook <obooklage@free.fr>
2024-07-18 00:46:49 +0200

    HTML Styles updated

--- src/informations.py
+++ src/informations.py
@@ -5,7 +5,7 @@ Created on Wed Jun  5 18:19:14 2024
 @author: obooklage
 """
 
-from PySide6.QtWidgets import QDialog, QTextBrowser
+from PySide6.QtWidgets import QDialog
 from ui_informations import Ui_DialogInformation
 import platform
 from settings import ClassSettings
@@ -18,15 +18,23 @@ class InformationsDlg(Ui_DialogInformation, QDialog):
         self.setFixedSize(400,361)
         self.setWindowTitle("Informations")
         self.pushButton_Close.clicked.connect(self.quit)
+        style = " style='color:#FFFFFF;background-color:#333333;'"
         text = ""
-        text += f"SYSTEM<br>\n{platform.system()}<br>\n<br>\n"
-        text += f"CONFIG FILE<br>\n{self.settings.GetConfigPath()}<br>\n<br>\n"
-        text += f"MIDIFILES PATH<br>\n{self.settings.GetMidiPath()}<br>\n<br>\n"
-        text += "HUMANIZE<br>\ncontrol_change:control 71 (set your midi-device)<br>\n<br>\n"
-        text += "SPEED CONTROL<br>\ncontrol_change:control 76 (set your midi-device)<br>\n<br>\n"
-        text += "MIDIFILE SELECT<br>\ncontrol_change:control 77 (set your midi-device)<br>\n<br>\n"
-        text += "MODE TOGGLE PLAYBACK/PASSTHROUGH<br>\ncontrol_change:control 51 (set your midi-device)<br>\n<br>\n"
-        text += "PROJECT : <a href='https://github.com/obook/I-like-Chopin'>https://github.com/obook/I-like-Chopin</a>"
+        text += f"<p{style}>SYSTEM</p>"
+        text += f"{platform.system()}"
+        text += f"<p{style}>CONFIG FILE</p>"
+        text += f"{self.settings.GetConfigPath()}"
+        text += f"<p{style}>MIDIFILES PATH</p>"
+        text += f"{self.settings.GetMidiPath()}"
+        text += f"<p{style}>HUMANIZE</p>"
+        text += "control_change:control 71 (set your midi-device)"
+        text += f"<p{style}>SPEED CONTROL</p>"
+        text += "control_change:control 76 (set your midi-device)"
+        text += f"<p{style}>MIDIFILE SELECT</p>"
+        text += "control_change:control 77 (set your midi-device)"
+        text += f"<p{style}>MODE TOGGLE PLAYBACK / PASSTHROUGH</p>"
+        text += "control_change:control 51 (set your midi-device)"
+        text += "<p style='color:#FF8888;'>PROJECT : <a href='https://github.com/obook/I-like-Chopin'>https://github.com/obook/I-like-Chopin</a></p>"
         self.textBrowser.setAcceptRichText(True)
         self.textBrowser.setOpenLinks(False)
         self.textBrowser.insertHtml(text)
