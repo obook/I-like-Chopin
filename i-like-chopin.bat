7542333333717f65bf7af704cc644c9d94217f54
obook <obooklage@free.fr>
2024-08-06 03:44:01 +0200

    Check poetry

--- i-like-chopin.bat
+++ i-like-chopin.bat
@@ -1,11 +1,18 @@
 @echo off
 python --version 3>NUL
 if errorlevel 1 goto errorNoPython
+poetry --version 3>NUL
+if errorlevel 1 goto errorNoPoetry
 cd src
 poetry run python i-like-chopin.py
 goto:eof
 :errorNoPython
 echo.
 echo Error^: Install python3 from https://www.python.org/
+set /p DUMMY=Press any key...
+goto:eof
+:errorNoPython
+echo.
+echo Error^: Install poetry by 'pip install poetry'
 set /p DUMMY=Press any key...
 :eof
