8403d4ff70d2c1e92f35f6b95e74a1485da4468a
obook <obooklage@free.fr>
2024-08-07 21:13:47 +0200

    Update songs-info.py

--- tools/songs-info.py
+++ tools/songs-info.py
@@ -4,6 +4,7 @@
 Created on Wed Aug  7 14:17:57 2024
 @author: obooklage
 Tool for checking midifiles : only channel 1, readeable
+! Channels are from 0 to 15
 """
 import os
 import glob
@@ -11,22 +12,22 @@ import pathlib
 from mido import MidiFile
 from collections import namedtuple
 
+indexes = {"tracks": 0, "duration": 1, "notes_on_channels": 2}
+Song = namedtuple("Song", ["tracks", "duration", "notes_on_channels"])
 
-indexes = {'tracks':0,'duration':1,'notes_on_channels':2}
-Song = namedtuple('Song', ['tracks', 'duration', 'notes_on_channels'])
 
 def MidiSongInfo(file):
     tracks = []
     duration = 0
     total_notes_on = 0
-    channels_notes_on = {} #0-16
+    channels_notes_on = {}
 
     # Tracks Informations
 
     try:
-        midi = MidiFile(file) # , debug=True  # PUT IN MIDI_SONG
+        midi = MidiFile(file)  # , debug=True  # PUT IN MIDI_SONG
     except:
-        return False # Bad Midifile (value > 127 ?)
+        return False  # Bad Midifile (value > 127 ?)
 
     duration = midi.length / 60
     for i, track in enumerate(midi.tracks):
@@ -36,15 +37,18 @@ def MidiSongInfo(file):
     for msg in MidiFile(file):
         if msg.type == "note_on":
             total_notes_on += 1
-            key = str(msg.channel)
+            key = int(msg.channel)
             if not key in channels_notes_on.keys():
-                channels_notes_on[key] = 0
-            channels_notes_on[key] += 1
+                channels_notes_on[int(key)] = 0
+            channels_notes_on[int(key)] += 1
 
-    channels_notes_on = dict(sorted(channels_notes_on.items())) # bug str sort 0,1,10,2,..
+    channels_notes_on = dict(
+        sorted(channels_notes_on.items())
+    )
     S = Song(tracks, duration, channels_notes_on)
 
     return S
+
 
 def ScanFiles(midipath):
     midifiles_dict = {}
@@ -67,32 +71,37 @@ def ScanFiles(midipath):
             list = midifiles_dict[path.parent.name]
             list.append(file)
         midifiles_raw.append(file)
-        midifiles_count +=1
+        midifiles_count += 1
 
-    return midifiles_dict #, midifiles_raw, midifiles_count
+    return midifiles_dict  # , midifiles_raw, midifiles_count
 
-folder = os.path.join(os.path.expanduser("~"), ".local","share","i-like-chopin","midiperso")
-files = ScanFiles(folder)
+
+folder = os.path.join(
+    os.path.expanduser("~"), ".local", "share", "i-like-chopin", "midiperso"
+)
+
+songs = ScanFiles(folder)
 
 print("** START")
 
-for key in files.keys():
-    print(f"Process {key}\n-------------")
-
-    list = files[key]
-    for midifile in sorted(list, key=lambda s: s.lower()):
+for artist in songs.keys():
+    print(f"Process {artist}\n-------------")
+    midifiles = songs[artist]
+    for midifile in sorted(midifiles, key=lambda s: s.lower()):
         midiname = pathlib.Path(midifile).stem
         Info = MidiSongInfo(midifile)
         if Info:
-            if '0' in Info[indexes['notes_on_channels']].keys():
+            channels = Info[indexes["notes_on_channels"]]  # dictionnary
+            if int(0) in channels.keys():
                 # remove key '0'
-                del Info[indexes['notes_on_channels']]["0"]
-                if Info[indexes['notes_on_channels']]: # D'autres channels actives
-                    print(f"OTHER CHANNELS WITH NOTES [{midiname}]")
+                save = channels.copy()
+                del channels[0]
+                if channels:  # other channels actives
+                    print(f"MULTICHANNELS [{midiname}] {save}")
 
             else:
                 print(f"WARNING no note channel 0 [{midifile}]")
         else:
-                print(f"WARNING error reading [{midifile}]")
+            print(f"ERROR reading [{midifile}] bad file?")
 
 print("** END")
