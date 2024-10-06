71ec8488ff22659b5286d3c21745f1e64d1fd09e
obook <obooklage@free.fr>
2024-10-06 11:08:28 +0200

    Update random-player.py

--- tools/random-player.py
+++ tools/random-player.py
@@ -5,10 +5,9 @@ Created on Sat Oct  5 11:01:21 2024
 @author: obooklage
 Make virt.env : python3.8 -m venv midoplayer
 source bin/activate
-Spyder : pip install spyder-kernels==3.0.*
+pip install spyder-kernels==3.0.* mido pynput
 Spyder run : bash -c '~/.local/spyder-6/envs/spyder-runtime/bin/spyder'
 Spyder Set Python : ~/midoplayer/bin/python
-pip install mido pynput
 
 Todo : sustain is not good count
 """
@@ -32,6 +31,7 @@ def on_release(key):
 
 
 def GetRamdom(files, device):
+    print("START GetRamdom")
     global next_song
     animation = ["-", "/", "-", "\\"]
     animation_index = 0
@@ -43,21 +43,26 @@ def GetRamdom(files, device):
         index = random.randint(0, len(files)-1)
         file = files[index]
         tracks, sustain = SystainPedalCheck(file)
-        if sustain > 0 and tracks <= 2:
-            print(f"midifile={file}, press Esc for next song...")
+        if sustain > 20 and tracks <= 3:
+            print(f"FOUND={file}, press Esc for next song...")
             print(f"tracks={tracks} sustain={sustain}")
             Play(file, device)
             next_song = False
             files.pop(index)
+        else:
+            print(f"REJECTED={file} tracks={tracks} sustain={sustain}")
 
 
 def Play(file, device):
     print("Playing...\r", end="")
     error_counter = 0
+    start_song = False
     # Player
     for msg in MidiFile(file):
-        #if msg.type == "note_on" or msg.type == "note_off":
-        time.sleep(msg.time)
+        if not start_song and msg.type == "note_on":
+            start_song = True
+        if start_song:
+            time.sleep(msg.time)
         try:
             device.send(msg)
         except Exception as error:
@@ -71,6 +76,7 @@ def Play(file, device):
 
 
 def SystainPedalCheck(file):
+    # print(f"START SystainPedalCheck {file}")
     sustain = 0
     tracks = 0
     try:
@@ -81,21 +87,19 @@ def SystainPedalCheck(file):
 
         for msg in MidiFile(file):
             if msg.type == "control_change":
-                # The sustain pedal sends CC 64 127
-                # and CC 64 0 messages on channel 1
-                if msg.value == 64:
-                    sustain += 1
+                sustain += 1
     except Exception as error:
         print(f"|!| CAN NOT READ {file} {error}")
         sustain = 0
         tracks = 0
+
+    # print(f"End of SystainPedalCheck, tracks={tracks} sustain={sustain}")
 
     return tracks, sustain
 
 
 my_path = os.path.expanduser("~/MUSIQUE")
 files = glob.glob(my_path + '/**/*.mid', recursive=True)
-index = random.randint(0, len(files))
 
 # Connect to synth
 device = open_output("Midi Through:Midi Through Port-0", autoreset=True)
