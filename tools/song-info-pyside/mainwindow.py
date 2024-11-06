5ddbc4c2f78de2cc9e609596611078c0d1445580
obook <obooklage@free.fr>
2024-11-06 20:23:48 +0100

    buggy

--- tools/song-info-pyside/mainwindow.py
+++ tools/song-info-pyside/mainwindow.py
@@ -1,16 +1,21 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import os
-from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
+from pathlib import Path, PurePath
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PySide6.QtCore import QEvent
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
-from songinfolib import MidiSong
+from songinfo import MidiSong
+from songgraph import graph_notes
 
-class MainWindow(QWidget):
+
+class MainWindow(QMainWindow):
+    ''' QT main window '''
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
@@ -18,43 +23,49 @@ class MainWindow(QWidget):
 
         self.setWindowTitle("MIDI Song info")
 
-        self.ui.pushButton_Load.clicked.connect(self.Load)
-        self.ui.pushButton_Quit.clicked.connect(self.Quit)
+        self.ui.pushButton_Load.clicked.connect(self.load_file)
+        self.ui.pushButton_Quit.clicked.connect(self.quit_application)
         self.setAcceptDrops(True)
         self.installEventFilter(self)  # drop files on readonly PlainTextEdit
 
-    def Load(self):
+    def load_file(self):
+        ''' file dialog '''
         fname = QFileDialog.getOpenFileName(
-                    self,
-                    "Open Midi File",
-                    "",
-                    "MIDI Files (*.mid) ;; All Files (*)",
+                self,
+                "Open Midi File",
+                "",
+                "MIDI Files (*.mid) ;; All Files (*)",
                 )
         if fname:
             file = fname[0]
-            self.PrintInfo(file)
+            self.print_info(file)
 
-    def PrintInfo(self, file):
-            name = os.path.basename(file)
-            self.setWindowTitle(name)
-            song = MidiSong(file)
-            duration = round(song.duration, 2)
-            tracks = song.tracks
-            sustain = song.sustain
-            set_tempo = song.set_tempo
+    def print_info(self, file):
+        ''' print informations in QPlainEditText '''
+        name = os.path.basename(file)
+        self.setWindowTitle(name)
+        song = MidiSong(file)
+        duration = round(song.duration, 2)
+        tracks = song.tracks
+        sustain = song.sustain
+        set_tempo = song.set_tempo
 
-            self.ui.plainTextEdit.clear()
+        self.ui.plainTextEdit.clear()
 
-            self.ui.plainTextEdit.appendPlainText(f"** File : {name}")
-            self.ui.plainTextEdit.appendPlainText(f"** Sustain : {sustain}")
-            self.ui.plainTextEdit.appendPlainText(f"** Tempo change : {set_tempo}")
-            self.ui.plainTextEdit.appendPlainText(f"** Duration : {duration} min.")
-            self.ui.plainTextEdit.appendPlainText("** TRACKS **")
-            for index in range(len(tracks)):
-                track = tracks[index]
-                self.ui.plainTextEdit.appendPlainText(f"{index} {track}")
+        self.ui.plainTextEdit.appendPlainText(f"** File : {name}")
+        self.ui.plainTextEdit.appendPlainText(f"** Sustain : {sustain}")
+        self.ui.plainTextEdit.appendPlainText(f"** Tempo change : {set_tempo}")
+        self.ui.plainTextEdit.appendPlainText(f"** Duration : {duration} min.")
+        self.ui.plainTextEdit.appendPlainText("** TRACKS **")
+        for index in range(len(tracks)):
+            track = tracks[index]
+            self.ui.plainTextEdit.appendPlainText(f"{index} {track}")
 
-    def eventFilter(self, o, e):  # drop files
+        parent = PurePath(file).parent.name
+        graph_notes(file, parent)
+
+    def eventFilter(self, o, e):
+        ''' intercept files dropped '''
         if e.type() == QEvent.DragEnter:  # remember to accept the enter event
             e.acceptProposedAction()
             return True
@@ -62,18 +73,24 @@ class MainWindow(QWidget):
             data = e.mimeData()
             urls = data.urls()
             if urls and urls[0].scheme() == "file":
-                self.PrintInfo(urls[0].toLocalFile())
+                self.print_info(urls[0].toLocalFile())
             return True
         return False  # remember to return false for other event types
 
     def closeEvent(self, event):  # overwritten
-        self.Quit()
+        ''' Windows closed by desktop '''
+        print("closeEvent")
+        self.quit_application()
 
-    def Quit(self):
+    def quit_application(self):
+        ''' The end '''
+        print("quit_application")
+        # self.deleteLater()
         QApplication.quit()
 
 
 if __name__ == "__main__":
+    # BUG : fnot closed under Qt Python virtual env
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
---
+++ tools/song-info-pyside/songinfo.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 14:17:57 2024
+@author: obooklage
+! Channels are from 0 to 15
+"""
+from mido import MidiFile, MetaMessage
+
+
+class MidiSong:
+    '''Class for midi file informations'''
+    tracks = []
+    duration = 0
+    total_notes_on = 0
+    channels_notes_on = {}
+    sustain = 0
+
+    def __init__(self, file):
+        self.file = file
+        self.tracks = []
+        self.duration = 0
+        self.total_notes_on = 0
+        self.channels_notes_on = {}
+        self.sustain = 0
+        self.set_tempo = 0
+        self.GetInfo()
+
+    def GetInfo(self):
+        ''' Get midi file informations'''
+        # Tracks Informations
+
+        try:
+            midi = MidiFile(self.file)  # , debug=True  # PUT IN MIDI_SONG
+        except:
+            return False  # Bad Midifile (value > 127 ?)
+
+        self.duration = midi.length / 60
+        for i, track in enumerate(midi.tracks):
+            self.tracks.append(track.name)
+
+        # Notes in selected channels (0-15)
+        for msg in MidiFile(self.file):
+
+            if msg.type == "note_on":
+                if msg.velocity:
+                    self.total_notes_on += 1
+                    key = int(msg.channel)
+                    if key not in self.channels_notes_on.keys():
+                        self.channels_notes_on[int(key)] = 0
+                    self.channels_notes_on[int(key)] += 1
+
+            if msg.type == "control_change":
+                # The sustain pedal sends CC 64 127
+                # and CC 64 0 messages on channel 1
+                if msg.control == 64:  # bug fixed, is not value, but control
+                    self.sustain += 1
+
+            elif isinstance(msg, MetaMessage):
+                if msg.type == 'set_tempo':
+                    self.set_tempo += 1
+
+            # meta : set_tempo
+            # elif msg.control == 64:
+
+        self.channels_notes_on = dict(
+            sorted(self.channels_notes_on.items())
+        )
+
+        return True
