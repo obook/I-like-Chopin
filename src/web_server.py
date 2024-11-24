0a5a394825b9abea85034248eaf7a21acc467f9e
obook <obooklage@free.fr>
2024-11-24 06:35:50 +0100

    Work about STOP bottle

--- src/web_server.py
+++ src/web_server.py
@@ -3,138 +3,81 @@
 """
 Created on Fri Nov 22 18:30:43 2024
 @author: obooklage
+@Todo : quiet do not works
 """
 
 import os
 import uuid
 import json
-from bottle import Bottle, static_file, response, request, redirect, abort
+from threading import Thread
+from bottle import run, route, static_file, response, request, redirect, abort, ServerAdapter
 from PySide6.QtCore import QThread
 from web_network import ClassWebNetwork
 
-# Project :
-
 '''
-########################################################################
-# Run a WSGI application in a daemon thread
-
-import bottle
-import threading
-import socket
-import time as _time
-
-class Server(bottle.WSGIRefServer):
-    def run(self, handler): # pragma: no cover
-        from wsgiref.simple_server import make_server, WSGIRequestHandler
-        if self.quiet:
-            base = self.options.get('handler_class', WSGIRequestHandler)
-            class QuietHandler(base):
-                def log_request(*args, **kw): pass
-            self.options['handler_class'] = QuietHandler
-        self.srv = make_server(self.host, self.port, handler, **self.options)
-        self.srv.serve_forever(poll_interval=0.1)
-
-def start_bottle_server(app, port, **kwargs):
-    server_thread = ServerThread(app, port, kwargs)
-    server_thread.daemon = True
-    server_thread.start()
-
-    ok = False
-    for i in range(10):
-        try:
-            conn = socket.create_connection(('127.0.0.1', port), 0.1)
-        except socket.error as e:
-            _time.sleep(0.1)
-        else:
-            conn.close()
-            ok = True
-            break
-    if not ok:
-        import warnings
-        warnings.warn('Server did not start after 1 second')
-
-    return server_thread.server
-
-class ServerThread(threading.Thread):
-    def __init__(self, app, port, server_kwargs):
-        threading.Thread.__init__(self)
-        self.app = app
-        self.port = port
-        self.server_kwargs = server_kwargs
-        self.server = Server(host='localhost', port=self.port, **self.server_kwargs)
-
-    def run(self):
-        bottle.run(self.app, server=self.server, quiet=True)
-
-def app_runner_setup(*specs):
-    Returns setup and teardown methods for running a list of WSGI
-    applications in a daemon thread.
-
-    Each argument is an (app, port) pair.
-
-    Return value is a (setup, teardown) function pair.
-
-    The setup and teardown functions expect to be called with an argument
-    on which server state will be stored.
-
-    Example usage with nose:
-
-    >>> setup_module, teardown_module = \
-        webracer.utils.runwsgi.app_runner_setup((app_module.app, 8050))
-
-
-    def setup(self):
-        self.servers = []
-        for spec in specs:
-            if len(spec) == 2:
-                app, port = spec
-                kwargs = {}
-            else:
-                app, port, kwargs = spec
-            self.servers.append(start_bottle_server(app, port, **kwargs))
-
-    def teardown(self):
-        for server in self.servers:
-            server.srv.shutdown()
-
-    return [setup, teardown]
-
-########################################################################
-# Not used
+From original bottle.py code
+Homepage and documentation: http://bottlepy.org/
+Copyright (c) 2009-2024, Marcel Hellkamp.
+License: MIT (see LICENSE for details)
+'''
 class MyWSGIRefServer(ServerAdapter):
-    server = None
+    quiet = True  # ADD SILENT MODE.
+    def run(self, app):  # pragma: no cover
+        from wsgiref.simple_server import make_server
+        from wsgiref.simple_server import WSGIRequestHandler, WSGIServer
+        import socket
 
-    def run(self, handler):
-        from wsgiref.simple_server import make_server, WSGIRequestHandler
-        if self.quiet:
-            class QuietHandler(WSGIRequestHandler):
-                def log_request(*args, **kw): pass
-            self.options['handler_class'] = QuietHandler
-        self.server = make_server(self.host, self.port, handler, **self.options)
-        self.server.serve_forever()
+        class FixedHandler(WSGIRequestHandler):
+            def address_string(self):  # Prevent reverse DNS lookups please.
+                return self.client_address[0]
 
-    def stop(self):
-        # self.server.server_close() <--- alternative but causes bad fd exception
-        self.server.shutdown()
+            def log_request(*args, **kw):
+                if not self.quiet:
+                    return WSGIRequestHandler.log_request(*args, **kw)
 
-'''
+        handler_cls = self.options.get('handler_class', FixedHandler)
+        server_cls = self.options.get('server_class', WSGIServer)
 
-# Used, but never stop
-class BottleServer:
+        if ':' in self.host:  # Fix wsgiref for IPv6 addresses.
+            if getattr(server_cls, 'address_family') == socket.AF_INET:
+
+                class server_cls(server_cls):
+                    address_family = socket.AF_INET6
+
+        self.srv = make_server(self.host, self.port, app, server_cls,
+                               handler_cls)
+        self.port = self.srv.server_port  # update port actual port (0 means random)
+        try:
+            self.srv.serve_forever()
+        except KeyboardInterrupt:
+            self.srv.server_close()  # Prevent ResourceWarning: unclosed socket
+            raise
+
+    def shutdown(self):  # ADD SHUTDOWN METHOD.
+        self.srv.server_close()
+        self.srv.shutdown()
+
+# VERY DURTY !
+_MyBottleServerSetup = {"uuid": None, 'parent': None}
+
+class MyBottleServer:
     def __init__(self, host, port, parent):
+        global _MyBottleServerSetup
         self.uuid = uuid.uuid4()
-        print(f"BottleServer {self.uuid} started")
-        self._host = host
-        self._port = port
+        print(f"MyBottleServer {self.uuid} started")
         self.pParent = parent
-        self._app = Bottle()
-        self._route()
+        _MyBottleServerSetup['uuid'] = self.uuid
+        _MyBottleServerSetup['parent'] = parent
+        # self._app = Bottle()
+        # self._route()
+        self.server = MyWSGIRefServer(host=host, port=port)
+        Thread(target=self.begin).start()
 
     def __del__(self):
-        print(f"BottleServer {self.uuid} destroyed")
+        print(f"MyBottleServer {self.uuid} destroyed")
 
     def _route(self):
-        self._app.route('/', method="GET", callback=self._index)
+        self._app.route('/', callback=self._index)
         self._app.route('/static/<filepath:path>',callback=self._server_static)
         # self._app.route('/midi/<filepath:path>',callback=self._server_midi)
         self._app.route('/status.json',callback=self._status)
@@ -145,23 +88,28 @@ class BottleServer:
         self._app.route('/player',callback=self._player)
         self._app.route('/score',callback=self._score)
 
-    def start(self):
-            # BLOCKING !
-            self.server = self._app.run(host=self._host, port=self._port, debug=True, quiet=True)
+    def begin(self):
+        run(server=self.server)
 
-    def _index(self):
+    def end(self):
+        self.server.shutdown()
+
+    @route('/')
+    def index():
         redirect("/static/index.html")
 
-    def _server_static(self, filepath):
-        uipath = self.pParent.Settings.GetUIPath()
+    @route('/static/<filepath:path>')
+    def server_static(filepath):
+        global _MyBottleServerSetup
+        uipath = "/home/obooklage/Documents/GitHub/I-like-Chopin/src/ui/"
         return static_file(filepath, root=uipath)
-    '''
-    def _server_midi(self, filepath):
-        midipath = self.pParent.Settings.GetMidiPath()
-        return static_file(filepath, root=midipath)
-    '''
-    def _status(self):
-        midisong = self.pParent.Midi.GetMidiSong()
+
+    @route('/status.json')
+    def _status():
+        global _MyBottleServerSetup
+        uuid = _MyBottleServerSetup['uuid']
+        pParent =  _MyBottleServerSetup['parent']
+        midisong = pParent.Midi.GetMidiSong()
         if midisong:
             status = {
                      "uuid":str(midisong.Getuuid()),
@@ -181,58 +129,78 @@ class BottleServer:
         try:
             return json.dumps(status)  # crash then pgm shutdown
         except Exception as error:
-            print(f"|!| BottleServer {self.uuid} error send status {error}")
+            print(f"|!| BottleServer {uuid} error send status {error}")
 
-    def _interfaces(self):
-        Network = ClassWebNetwork(self.pParent)
+    @route('/interfaces.json')
+    def _interfaces():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
+        Network = ClassWebNetwork(pParent)
         interfaces = Network.GetWebUrls()
         response.content_type = 'application/json'
         return json.dumps(interfaces)
 
-    def _files(self):
-        file_dic = self.pParent.Midifiles.GetFiles()
+    @route('/files.json')
+    def _files():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
+        file_dic = pParent.Midifiles.GetFiles()
         response.content_type = 'application/json'
         return json.dumps(file_dic)
 
-    def _play(self):
+    @route('/play')
+    def _play():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
+        uuid = _MyBottleServerSetup['uuid']
         midifile = request.query.song
-        print(f"BottleServer {self.uuid} request [{midifile}]")
-        if self.pParent:
+        print(f"BottleServer {uuid} request [{midifile}]")
+        if pParent:
              try:
-                 self.pParent.MidifileChange(midifile)  # DANGEROUS ?
+                 pParent.MidifileChange(midifile)  # DANGEROUS ?
              except:
                  pass
 
-    def _do(self):
+    @route('/do')
+    def _do():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
         action = request.query.action
         print(f"REQUEST DO={action}")
-        if self.pParent and action == "stop":
+        if pParent and action == "stop":
             try:
-                self.pParent.Midi.StopPlayer()  # DANGEROUS ?
+                pParent.Midi.StopPlayer()  # DANGEROUS ?
             except:
                 pass
-        elif self.pParent and action == "shuffle":  # ex "next"
+        elif pParent and action == "shuffle":  # ex "next"
             try:
-                self.pParent.ShuffleMidifile()  # DANGEROUS ?
+                pParent.ShuffleMidifile()  # DANGEROUS ?
             except:
                 pass
-        elif self.pParent and action == "replay":
+        elif pParent and action == "replay":
             try:
-                self.pParent.MidifileReplay()  # DANGEROUS ?
+                pParent.MidifileReplay()  # DANGEROUS ?
             except:
                 pass
 
-    def _player(self):
+    @route('/player')
+    def _player():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
         mode = request.query.mode
         print(f"REQUEST MODE={mode}")
         try:
-            self.pParent.ChangePlayerMode(mode)  # DANGEROUS ?
+            pParent.ChangePlayerMode(mode)  # DANGEROUS ?
         except:
             pass
 
-    def _score(self):
+    @route('/score')
+    def _score():
+        global _MyBottleServerSetup
+        pParent =  _MyBottleServerSetup['parent']
+        uuid = _MyBottleServerSetup['uuid']
         pdf = request.query.pdf
-        file = os.path.join(self.pParent.Settings.GetMidiPath(), pdf)
+        file = os.path.join(pParent.Settings.GetMidiPath(), pdf)
         if os.path.isfile(file):
             f = open(file, 'rb')
             data = f.read()
@@ -240,7 +208,7 @@ class BottleServer:
             response.content_type = 'application/pdf'
             return data
         else:
-            print(f"|!| BottleServer {self.uuid} request score {file} note exists")
+            print(f"|!| BottleServer {uuid} request score {file} note exists")
             abort(404, "Sorry, file not found.")
 
 class ClassWebServer(QThread):
@@ -251,6 +219,7 @@ class ClassWebServer(QThread):
     def __init__(self, parent):
         QThread.__init__(self)
         self.uuid = uuid.uuid4()
+        print(f"WebServer {self.uuid} started")
         self.pParent = parent
         self.Settings = parent.Settings
         self.port = self.Settings.GetServerPort()
@@ -259,21 +228,8 @@ class ClassWebServer(QThread):
         print(f"WebServer {self.uuid} destroyed")
 
     def run(self):
-        self.server = BottleServer(host="0.0.0.0", port=self.port, parent=self.pParent)
-        # print(f"**** DEBUG={self.server}")
-        self.server.start()
-        '''
-        self.server_process = Process(target=self.startserver)
-        self.server_process.start()
+        self.server = MyBottleServer(host="0.0.0.0", port=self.port, parent=self.pParent)
 
-
-    def startserver(self):
-        print(f"WebServer {self.uuid} start")
-        self.server = BottleServer(host="0.0.0.0", port=self.port, parent=self.pParent)
-        self.server.start()
-        print(f"WebServer {self.uuid} stop")
-        '''
     def stop(self):
         print(f"WebServer {self.uuid} stop")
-        # self.server_process.terminate()
-        pass
+        self.server.end()
