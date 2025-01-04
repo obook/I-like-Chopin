ec97416b7c9859d350220eae09d6fb399b82e091
obook <obooklage@free.fr>
2025-01-04 22:21:16 +0100

    Remove systray tests

--- src/_mainwindow_menu.py
+++ src/_mainwindow_menu.py
@@ -4,11 +4,7 @@
 Created on Tue Dec 31 10:23:12 2024
 @author: obooklage
 """
-# from PySide6.QtGui import QAction
 
-import os
-from PySide6.QtGui import QIcon, QAction
-from PySide6.QtWidgets import QSystemTrayIcon, QMenu
 
 class menu:
 
@@ -24,36 +20,6 @@ class menu:
             action = action_favorites.addAction(title)
             action.setData(dict['path'])
         action_favorites.triggered.connect(self.TriggeredFavorite)
-
-        '''
-        # Systray
-        ICON_SYSTRAY = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "icons", "svg", "i-like-chopin.svg"
-        )
-
-        icon = QIcon(ICON_SYSTRAY)
-
-        # Create the tray
-        tray = QSystemTrayIcon()
-        tray.setIcon(icon)
-        tray.setVisible(True)
-
-
-
-        # Create the menu
-        menu = QMenu()
-        action = QAction("Open Main Window")
-        menu.addAction(action)
-
-        # Add a Quit option to the menu.
-        quit = QAction("Quit")
-        quit.triggered.connect(self.TriggeredActionQuit())
-        menu.addAction(quit)
-
-        # Add the menu to the tray
-        tray.setContextMenu(menu)
-        tray.setToolTip("I like Chopin")
-        '''
 
     def TriggeredActionQuit(self):
         self.Quit()
