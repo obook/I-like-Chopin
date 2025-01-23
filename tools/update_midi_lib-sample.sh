15f007dd7d8da7d0ec39ea495c5d3e91a37c64a7
øbook <obooklage@free.fr>
2025-01-23 13:48:36 +0100

    Create update_midi_lib-sample.sh

---
+++ tools/update_midi_lib-sample.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Use only bash, not sh
+# Author : obooklage
+# Date : 23/01/2025
+# Desc1 : check names compatibles for Windows
+# Desc2 : create music score from midifiles
+# A venv must be created
+
+midipath=$HOME"/Documents/GitHub/midi/"
+
+venv_path=$HOME"/PythonEnv/mido"
+venv_activate=$venv_path"/bin/activate"
+venv_python3=$venv_path"/bin/python3"
+
+# update midilib
+/usr/bin/git pull $midipath
+
+# Activate venv
+source $venv_activate
+
+# Check filenames
+cd ~/Documents/GitHub/I-like-Chopin/tools
+$venv_python3 rename-windows.py $midipath
+
+# Make music scores
+cd ~/Documents/GitHub/I-like-Chopin/tools/song-score
+$venv_python3 song-score-scan.py $midipath
+
+# Deactivate venv
+deactivate
