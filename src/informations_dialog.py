b30ba90211af043c5a132bcd4df2f302412df8ba
obook <obooklage@free.fr>
2025-10-01 14:00:25 +0200

    MAJ.

--- src/informations_dialog.py
+++ src/informations_dialog.py
@@ -49,8 +49,8 @@ class InformationsDlg(Ui_DialogInformation, QDialog):
             svgWidget = QSvgWidget()
             svgWidget.setStyleSheet("QSvgWidget {background-color:white;}")
             svgWidget.load(QByteArray(qrcode.encode()))
-            svgWidget.setFixedWidth(120)  # 134 = trop gros pour Raspberry ?
-            svgWidget.setFixedHeight(120)  # 134 = trop gros pour Raspberry ?
+            svgWidget.setFixedWidth(80)  # 134 = trop gros pour Raspberry ?
+            svgWidget.setFixedHeight(80)  # 134 = trop gros pour Raspberry ?
 
             self.formLayout.addRow(svgWidget)
             self.formLayout.addRow(QLabel(""))
